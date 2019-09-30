40462ada5f44ab0ea040a4fece09d344ace8fe52
evanmacbride <ejmacbride@gmail.com>
2019-09-29 22:02:48 -0400

    change date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,70 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+export default ({ data }) => {
+  //console.log(data.allMarkdownRemark.edges[0])
+  return (
+    <Layout>
+      <div>
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Top Posts from Reddit
+        </h1>
+        <article>
+          <h2>The Latest</h2>
+          <h3>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h3>
+          <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }} />
+        </article>
+        <h4>{data.allMarkdownRemark.totalCount - 1} Older Posts</h4>
+        {data.allMarkdownRemark.edges.slice(1).map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+            `}>
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{" "}
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+export const query = graphql`
+  query {
+    allMarkdownRemark (sort: {
+      fields: [frontmatter___date],
+      order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+          html
+        }
+      }
+    }
+  }
+`
