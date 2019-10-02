bb011a67262f68bb2de6e6c3b184d7af2dfcb03b
evanmacbride <ejmacbride@gmail.com>
2019-10-02 12:32:02 -0400

    put all md posts in posts folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({ node, getNode, basePath: `pages`})
+    const slug = createFilePath({ node, getNode, basePath: `pages/posts`})
     createNodeField({
       node,
       name: `slug`,
