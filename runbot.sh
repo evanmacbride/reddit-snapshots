5a1322ef36af00610fdace1ddd0f9a92c7dea0e1
evanmacbride <ejmacbride@gmail.com>
2019-10-02 14:06:51 -0400

    add scripts to auto-update with new md files

---
+++ runbot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=$PATH:/usr/bin
+#$PATH/python3 /home/chrx/Projects/reddit-bot/bot.py
+#python3 /home/chrx/Projects/reddit-digest/hello.py
+python3 $PATH_TO_PROJECT_DIR/bot.py
---
+++ update-posts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "start runbot.sh"
+source $PATH_TO_PROJECT_DIR/runbot.sh
+echo "finish runbot.sh"
+echo "start md-git.sh"
+source $PATH_TO_PROJECT_DIR/md-git.sh
+echo "finish md-git.sh"
