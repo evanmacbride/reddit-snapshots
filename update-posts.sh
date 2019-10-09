cce164a6cce9243f2358b44d28fdf3ee6c022518
evanmacbride <ejmacbride@gmail.com>
2019-10-08 22:29:17 -0400

    add new md files

--- update-posts.sh
+++ update-posts.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-echo "start runbot.sh"
+echo "pulling from GitHub"
+git pull https://$YOUR_GITHUB_USERNAME:$YOUR_GITHUB_PASSWORD@github.com/$YOUR_GITHUB_USERNAME/reddit-digest.git
+echo "start runbot.sh - create new md file based on Reddit posts"
 source $PATH_TO_PROJECT_DIR/runbot.sh
 echo "finish runbot.sh"
-echo "start md-git.sh"
+echo "start md-git.sh - commit and push to GitHub"
 source $PATH_TO_PROJECT_DIR/md-git.sh
 echo "finish md-git.sh"
