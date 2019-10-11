bace564d55cf16aec9a839c0b6da5ecc5d34dec0
evanmacbride <ejmacbride@gmail.com>
2019-10-10 22:42:05 -0400

    fix update script bug

--- update-posts.sh
+++ update-posts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "pulling from GitHub"
+source /etc/environment
+cd $PATH_TO_PROJECT_DIR/runbot.sh
 git pull https://$YOUR_GITHUB_USERNAME:$YOUR_GITHUB_PASSWORD@github.com/$YOUR_GITHUB_USERNAME/reddit-digest.git
 echo "start runbot.sh - create new md file based on Reddit posts"
 source $PATH_TO_PROJECT_DIR/runbot.sh
