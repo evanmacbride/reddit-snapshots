e2307d62e1b8375330128857f3b941bc3bc831db
evanmacbride <ejmacbride@gmail.com>
2019-10-17 01:10:06 -0400

    add update-scripts folder

---
+++ update-scripts/runbot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=$PATH:/usr/bin
+#$PATH/python3 /home/chrx/Projects/reddit-bot/bot.py
+#python3 /home/chrx/Projects/reddit-digest/hello.py
+source /etc/environment
+python3 $PATH_TO_PROJECT_DIR/update-scripts/bot.py
