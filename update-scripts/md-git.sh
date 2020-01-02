daad3364790767330f301e6100a4fb26671d3c72
evanmacbride <ejmacbride@gmail.com>
2020-01-01 20:59:10 -0500

    implement Post class with getAdjustedScore function, build snapshots from heaps

--- update-scripts/md-git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Add all new markdown files, commit, and push to GitHub.
-# Username and password are stored as environment variables.
-cd $PATH_TO_PROJECT_DIR
-git add src/pages/posts/*.md
-git commit -am "add new md files"
-git push https://$YOUR_GITHUB_USERNAME:$YOUR_GITHUB_PASSWORD@github.com/$YOUR_GITHUB_USERNAME/reddit-digest.git master
