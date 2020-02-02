ba9681fe67905d9a706d11b8db6267673d04b735
evanmacbride <ejmacbride@gmail.com>
2020-02-02 15:15:17 -0500

    add footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -34,15 +34,56 @@ export default ({ children }) => {
 
         `}
       >
-        <h3
+        <nav
           css={css`
-            color: #d2dae2e4;
+            display: flex;
+            flex-flow: row wrap;
+            justify-content: space-between;
             margin: 0 auto;
             max-width: 610px;
-            padding: 0 5px 0 5px;
-          `}>
-          {data.site.siteMetadata.title}
-        </h3>
+        `}>
+          <Link to={`/`}>
+          <h3
+            css={css`
+
+              margin: 0 auto;
+              padding: 0 5px 0 5px;
+            `}>
+            {data.site.siteMetadata.title}
+          </h3>
+          </Link>
+          <span
+            css={css`
+              display: flex;
+              flex-flow: row wrap;
+            `}
+          >
+            <Link to={`/archive`}
+              css={css`
+                  margin-right: 0.58em;
+              `}
+            >
+              <h3
+                css={css`
+                  color: #d2dae2e4;
+                  margin-bottom: 0;
+                `}
+              >
+                Archive
+              </h3>
+            </Link>
+              <Link to={`/about`}>
+                <h3
+                  css={css`
+                    color: #d2dae2e4;
+                    margin-bottom: 0;
+                  `}
+                >
+                  About
+                </h3>
+              </Link>
+          </span>
+        </nav>
       </section>
     </footer>
       {children}
